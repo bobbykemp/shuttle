a5fb1358a88802bbcb08c5a1799b2324515f955c
Bobby Kemp <rkemp@herzog.com>
2022-01-04 16:09:46 -0600

    changed look and feel of app to windows added method to create and show sys tray icon with options added method to create and show a select folder popup added method to create image from resource path added directorychooser class for selecting folder to monitor added default bulb image for tray icon

--- src/main/java/com/rjkemp/shuttle/App.java
+++ src/main/java/com/rjkemp/shuttle/App.java
@@ -1,13 +1,112 @@
 package com.rjkemp.shuttle;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+import java.awt.*;
+import java.awt.event.*;
+import java.net.URL;
+import javax.swing.*;
+import java.io.File;
+
+public class App {
+    static File watchedDirectory;
+
+    public static void main(String[] args) {
+        try {
+            UIManager.setLookAndFeel("com.sun.java.swing.plaf.windows.WindowsLookAndFeel");
+            // UIManager.setLookAndFeel("javax.swing.plaf.metal.MetalLookAndFeel");
+        } catch (UnsupportedLookAndFeelException ex) {
+            ex.printStackTrace();
+        } catch (IllegalAccessException ex) {
+            ex.printStackTrace();
+        } catch (InstantiationException ex) {
+            ex.printStackTrace();
+        } catch (ClassNotFoundException ex) {
+            ex.printStackTrace();
+        }
+        /* Turn off metal's use of bold fonts */
+        UIManager.put("swing.boldMetal", Boolean.FALSE);
+        // Schedule a job for the event-dispatching thread:
+        // adding TrayIcon.
+        SwingUtilities.invokeLater(new Runnable() {
+            public void run() {
+                createAndShowTrayItem();
+            }
+        });
+    }
+
+    private static void createAndShowTrayItem() {
+        // Check the SystemTray support
+        if (!SystemTray.isSupported()) {
+            System.out.println("SystemTray is not supported");
+            return;
+        }
+        final PopupMenu popup = new PopupMenu();
+        final TrayIcon trayIcon = new TrayIcon(createImage("images/bulb.gif", "tray icon"));
+        final SystemTray tray = SystemTray.getSystemTray();
+
+        // Create a popup menu components
+        MenuItem pickFolder = new MenuItem("Select a folder");
+        MenuItem exitItem = new MenuItem("Exit");
+
+        // Add components to popup menu
+        popup.add(pickFolder);
+        popup.addSeparator();
+        popup.add(exitItem);
+
+        trayIcon.setPopupMenu(popup);
+
+        try {
+            tray.add(trayIcon);
+        } catch (AWTException e) {
+            System.out.println("TrayIcon could not be added.");
+            return;
+        }
+
+        trayIcon.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                JOptionPane.showMessageDialog(null,
+                        "This dialog box is run from System Tray");
+            }
+        });
+
+        pickFolder.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                createAndShowSelectFolderPopup();
+            }
+        });
+
+        exitItem.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                tray.remove(trayIcon);
+                System.exit(0);
+            }
+        });
+    }
+
+    private static void createAndShowSelectFolderPopup() {
+        JFrame frame = new JFrame("");
+        final DirectoryChooser panel = new DirectoryChooser();
+        frame.addWindowListener(
+                new WindowAdapter() {
+                    public void windowClosing(WindowEvent e) {
+                        watchedDirectory = panel.selection;
+                        System.out.println(watchedDirectory);
+                    }
+                });
+
+        frame.getContentPane().add(panel, "Center");
+        frame.setSize(panel.getPreferredSize());
+        frame.setVisible(true);
+    }
+
+    // Obtain the image URL
+    protected static Image createImage(String path, String description) {
+        URL imageURL = App.class.getResource(path);
+
+        if (imageURL == null) {
+            System.err.println("Resource not found: " + path);
+            return null;
+        } else {
+            return (new ImageIcon(imageURL, description)).getImage();
+        }
     }
 }
---
+++ src/main/java/com/rjkemp/shuttle/DirectoryChooser.java
@@ -0,0 +1,54 @@
+package com.rjkemp.shuttle;
+
+import javax.swing.*;
+import java.awt.event.*;
+import java.awt.*;
+import java.io.File;
+
+public class DirectoryChooser extends JPanel
+        implements ActionListener {
+
+    JButton go;
+    JLabel label;
+    JFileChooser chooser;
+    String choosertitle = "Select a directory to watch for changes";
+    File selection;
+
+    private String getSelectedAbsPath() {
+        if (selection == null) {
+            return "No folder selected yet";
+        } else {
+            return selection.getAbsolutePath();
+        }
+    }
+
+    public DirectoryChooser() {
+        go = new JButton("Select Watched Directory");
+        label = new JLabel(getSelectedAbsPath());
+        go.addActionListener(this);
+        add(go);
+        add(label);
+    }
+
+    public void actionPerformed(ActionEvent e) {
+        chooser = new JFileChooser();
+        chooser.setCurrentDirectory(new java.io.File(System.getProperty("user.home")));
+        chooser.setDialogTitle(choosertitle);
+        chooser.setFileSelectionMode(JFileChooser.DIRECTORIES_ONLY);
+
+        // disable the "All files" option.
+        chooser.setAcceptAllFileFilterUsed(false);
+
+        if (chooser.showOpenDialog(this) == JFileChooser.APPROVE_OPTION) {
+            selection = chooser.getSelectedFile();
+        } else {
+            selection = null;
+        }
+
+        label.setText(getSelectedAbsPath());
+    }
+
+    public Dimension getPreferredSize() {
+        return new Dimension(200, 200);
+    }
+}
